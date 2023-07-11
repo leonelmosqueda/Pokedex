6b1af6f29709053ac836b7780ce2659f7db6a28a
leonelmosqueda <maurymosqueda@gmail.com>
2023-07-11 15:46:48 -0300

    Add mapper to transform API data into a Pokemon object

---
+++ src/mappers/pokedex.js
@@ -0,0 +1,10 @@
+import { Pokemon } from '../entities/pokemon.js';
+
+export function mapPokemon (apiData) {
+  const { id, name, height, weight, sprites, types, abilities } = apiData;
+  const urlImage = sprites.other['official-artwork'].front_default;
+  const typeNames = types.map((type) => type.type.name);
+  const abilityNames = abilities.map((ability) => ability.ability.name);
+
+  return new Pokemon(id, name, height, weight, urlImage, typeNames, abilityNames);
+}
