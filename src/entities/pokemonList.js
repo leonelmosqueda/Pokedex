c4c6adc99696c4c4a1189acd9f7b6ab7ceee9b35
leonelmosqueda <maurymosqueda@gmail.com>
2023-07-11 17:30:10 -0300

    Create PokemonList class

---
+++ src/entities/pokemonList.js
@@ -0,0 +1,14 @@
+class PokemonList {
+  /**
+   * @param {Number} total - The total number of pokemon in the pokedex.
+   * @param {String | null} next - The URL for the next page of results.  Can be null.
+   * @param {String | null} previous - The URL for the previous page of results. Can be null.
+   * @param {Array<String>} results - The array of results
+   */
+  constructor (total, next, previous, results) {
+    this.total = total;
+    this.next = next;
+    this.previous = previous;
+    this.results = results;
+  }
+}
