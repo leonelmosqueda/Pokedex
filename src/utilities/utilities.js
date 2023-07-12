9d5d4100513d8c4b3ac84bce94c374d1c6825f52
leonelmosqueda <maurymosqueda@gmail.com>
2023-07-12 20:20:47 -0300

    Refactor Utilities folder to improve readability and maintainability

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -2,34 +2,40 @@ import { BASE_URL } from '../api/pokemon.js';
 import { POKEMON_PER_PAGE } from '../pokedex.js';
 
 export function getId (url) {
-  return url.slice(url.search('pokemon') + 8, url.length - 1);
+  const startIndex = url.search('pokemon') + 8;
+  const endIndex = url.length - 1;
+  return url.slice(startIndex, endIndex);
 }
 
-export function getUrl (offset) {
-  return `${BASE_URL}?offset=${offset}&limit=${POKEMON_PER_PAGE}`;
+export function getUrl (pageOffset) {
+  const url = `${BASE_URL}?offset=${pageOffset}&limit=${POKEMON_PER_PAGE}`;
+  return url;
 }
 
 /**
  * Formats the information of a Pokemon.
  *
  * @param {Object} pokemon - The Pokemon object to format.
- * @param {string} pokemon.name - The name of the Pokemon.
- * @param {number} pokemon.height - The height of the Pokemon in decimeters.
- * @param {number} pokemon.weight - The weight of the Pokemon in hectograms.
- * @param {string} pokemon.image - The URL of the Pokemon's image.
+ * @param {String} pokemon.name - The name of the Pokemon.
+ * @param {Number} pokemon.height - The height of the Pokemon in decimeters.
+ * @param {Number} pokemon.weight - The weight of the Pokemon in hectograms.
+ * @param {String} pokemon.image - The URL of the Pokemon's image.
  * @param {Array} pokemon.types - An array of strings representing the types of the Pokemon.
  * @param {Array} pokemon.abilities - An array of strings representing the abilities of the Pokemon.
- * @return {Object} - The formatted Pokemon object.
+ * @returns {Object} - The formatted Pokemon object.
  */
-export function formatPokemonInfo ({ name, height, weight, image, types, abilities }) {
+export function formatPokemonData (pokemon) {
+  const { name, height, weight, image, types, abilities } = pokemon;
   const typesString = types.join(' - ');
   const abilitiesString = abilities.join(' - ');
+  const formattedWeight = `${weight / 10} kg`;
+  const formattedHeight = `${height * 10} cm`;
 
   return {
     name,
     image,
-    weight: `${weight / 10} kg`,
-    height: `${height * 10} cm`,
+    weight: formattedWeight,
+    height: formattedHeight,
     types: typesString,
     abilities: abilitiesString
   };
