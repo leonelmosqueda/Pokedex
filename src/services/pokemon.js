69ae7530e2ac9152a81b68a44cd000f1a5d9025d
leonelmosqueda <maurymosqueda@gmail.com>
2023-07-12 17:37:27 -0300

    Refactor Services folder to improve readability and maintainability

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -11,12 +11,22 @@ import {
 } from '../api/pokemon.js';
 import { mapPokemon, mapPokemonList } from '../mappers/pokedex.js';
 
+/**
+ * Fetches a list of Pokemon based on the provided page number.
+ *
+ * @typedef { import('../entities/pokemonList').PokemonList } PokemonList
+ *
+ * @param {Number} page - The page number to fetch the Pokemon list from.
+ * @return {Promise<PokemonList>} pokemonList - A promise that resolves to an PokemonList object.
+ */
 export async function fetchPokemonList (page) {
   const limit = POKEMON_PER_PAGE;
   const offset = (page - 1) * limit;
+
   try {
-    return fetchPokemonListFromLocalStorage(offset, limit);
-  } catch (e) {
+    const pokemonList = await fetchPokemonListFromLocalStorage(offset, limit);
+    return pokemonList;
+  } catch (error) {
     const pokemonListData = await fetchPokemonListFromApi(page);
     const pokemonList = mapPokemonList(pokemonListData);
     savePokemonList(offset, limit, pokemonList);
@@ -24,14 +34,21 @@ export async function fetchPokemonList (page) {
   }
 }
 
-export async function fetchPokemonInfo (id) {
+/**
+ * Fetches pokemon data based on the given id.
+ * @typedef { import('../entities/pokemon').Pokemon } Pokemon
+ *
+ * @param {number} id - The id of the pokemon to fetch.
+ * @return {Promise<Pokemon>} The pokemon data.
+ */
+export async function fetchPokemonData (id) {
   if (id === undefined) {
     throw new Error('An id is needed to fetch a pokemon');
   }
 
   try {
-    return fetchPokemonInfoFromLocalStorage(id);
-  } catch (e) {
+    return await fetchPokemonInfoFromLocalStorage(id);
+  } catch (error) {
     const pokemonData = await fetchPokemonInfoFromApi(id);
     const pokemon = mapPokemon(pokemonData);
     savePokemon(id, pokemon);
