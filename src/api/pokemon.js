ff8d14467192dc10b1a20b8e556675248b67dfd0
leonelmosqueda <maurymosqueda@gmail.com>
2023-07-12 15:56:21 -0300

    Refactor API folder to improve readability and maintainability

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -5,15 +5,13 @@ export const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 export async function fetchPokemonList (page = 1) {
   const limit = POKEMON_PER_PAGE;
   const offset = (page - 1) * limit;
-  const url = `${BASE_URL}?offset=${offset}&limit=${limit}`;
-  const response = await fetch(url);
-  const data = response.json();
-  return data;
+
+  const response = await fetch(`${BASE_URL}?offset=${offset}&limit=${limit}`);
+
+  return await response.json();
 }
 
 export async function fetchPokemonInfo (id) {
-  const url = `${BASE_URL}/${id}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  const response = await fetch(`${BASE_URL}/${id}`);
+  return await response.json();
 }
